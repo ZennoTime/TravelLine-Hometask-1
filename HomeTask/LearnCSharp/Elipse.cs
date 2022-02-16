312e4fd60dd995519f729c56958a09f5fbbaee11
Vlad <osokin-vlad.osokin@yandex.ru>
2022-02-17 01:19:45 +0300

    TL Hometask 1

---
+++ HomeTask/LearnCSharp/Elipse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCSharp
+{
+    public class Elipse
+    {
+        public Point LeftTopPoint { get; private set; }
+        public Point RightBottomPoint { get; private set; }
+        public double Horizontal { get; private set; }
+        public double Vertical { get; private set; }
+
+        public Elipse(Point lefttopPoint, Point rightbottomPoint)
+        {  
+            LeftTopPoint = lefttopPoint;
+            RightBottomPoint = rightbottomPoint;
+            Horizontal = (RightBottomPoint.X - LeftTopPoint.X) / 2;
+            Vertical = (LeftTopPoint.Y - RightBottomPoint.Y) / 2;
+            if (Horizontal < 0)
+            {
+                throw new ArgumentException(
+                    "Горизонтальный радиус должен быть больше нуля"
+                );
+            }
+            if ( Vertical < 0)
+            {
+                throw new ArgumentException(
+                    "Вертикальный радиус должен быть больше нуля"
+                );
+            }
+        }
+        public double GetPerimeter()
+        {
+            double Perimeter = 2 * Math.PI * Math.Sqrt(((Math.Pow(Horizontal, 2)) + (Math.Pow(Vertical, 2))) / 2);
+            return (Perimeter);
+        }
+        public double GetSquare()
+        {
+            double Square = Horizontal * Vertical * Math.PI;
+            return (Square); 
+        }
+    }
+}
---
+++ HomeTask/LearnCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using LearnCSharp;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Point lefttoppoint = new Point
+        {
+            X = 10,
+            Y = 10
+        };
+        Point rightbottompoint = new Point
+        {
+            X = 20,
+            Y = 6
+        };
+        try
+        {
+            Elipse elipse = new Elipse(lefttoppoint, rightbottompoint);
+            double perimeter = elipse.GetPerimeter();
+            double square = elipse.GetSquare();
+            Console.WriteLine($"Длина дуги Элипса равна = {perimeter}");
+            Console.WriteLine($"Площадь эллипса = {square}");
+            Console.WriteLine($"Исходные координаты точек: {elipse.LeftTopPoint.ToString()} {elipse.RightBottomPoint.ToString()}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
